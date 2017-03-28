df4b87b77f9bbc949fc197e692c00dcb1ad9e9a9
Gary Burgess <gary.burgess@gmail.com>
2017-03-28 17:15:41 +0100

    Add effect row equality

--- test/Main.purs
+++ test/Main.purs
@@ -5,6 +5,7 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Newtype (class Newtype, unwrap)
 import Type.Equality (class TypeEquals, to, from)
+import Type.Row.Effect.Equality as REE
 
 newtype RecordNewtype = RecordNewtype
   { message :: String }
@@ -15,5 +16,11 @@ instance newtypeRecordNewtype ::
   wrap = RecordNewtype <<< to
   unwrap (RecordNewtype rec) = from rec
 
+class Foo f where
+  foo :: String -> f Unit
+
+instance fooEff :: REE.EffectRowEquals eff (console :: CONSOLE | e) => Foo (Eff eff) where
+  foo = REE.effFrom <<< log
+
 main :: Eff (console :: CONSOLE) Unit
-main = log (unwrap (RecordNewtype { message: "Done" })).message
+main = foo (unwrap (RecordNewtype { message: "Done" })).message
