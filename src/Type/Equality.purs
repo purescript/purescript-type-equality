be4bc6a11f0e4d711f86fd3de4da7a622370e7ee
Nicholas Scheel <monoidmusician@gmail.com>
2020-07-24 12:23:14 -0500

    Add leibnizOp

--- src/Type/Equality.purs
+++ src/Type/Equality.purs
@@ -3,6 +3,7 @@ module Type.Equality
   , to
   , from
   , leibniz
+  , leibnizOp
   ) where
 
 -- | This type class asserts that types `a` and `b`
@@ -15,12 +16,19 @@ module Type.Equality
 -- | Note: any instance will necessarily overlap with
 -- | `refl` below, so instances of this class should
 -- | not be defined in libraries.
--- class TypeEquals :: forall k. k -> k -> Constraint
+class TypeEquals :: forall k. k -> k -> Constraint
 class TypeEquals a b | a -> b, b -> a where
   leibniz :: forall p. p a -> p b
 
 instance refl :: TypeEquals a a where
   leibniz a = a
+
+newtype Op :: forall k. k -> k -> Type
+newtype Op a b = Op (forall p. p b -> p a)
+
+leibnizOp :: forall p a b. TypeEquals a b => p b -> p a
+leibnizOp = case leibniz (Op (\pa -> pa) :: Op a a) of
+  (Op f :: Op a b) -> f
 
 newtype To a = To a
 
@@ -28,8 +36,6 @@ to :: forall a b. TypeEquals a b => a -> b
 to a = case leibniz (To a) of
   To b -> b
 
-newtype From a b = From (b -> a)
-
 from :: forall a b. TypeEquals a b => b -> a
-from b = case leibniz (From (\a -> a) :: From a a) of
-  (From f :: From a b) -> f b
+from b = case leibnizOp (To b) of
+  To a -> a
