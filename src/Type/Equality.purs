246ff0bc5ce93d6ef72edd1f35b6ae78686531ed
Nicholas Scheel <monoidmusician@gmail.com>
2020-07-24 12:12:44 -0500

    Use leibniz equality

--- src/Type/Equality.purs
+++ src/Type/Equality.purs
@@ -2,6 +2,7 @@ module Type.Equality
   ( class TypeEquals
   , to
   , from
+  , leibniz
   ) where
 
 -- | This type class asserts that types `a` and `b`
@@ -14,10 +15,21 @@ module Type.Equality
 -- | Note: any instance will necessarily overlap with
 -- | `refl` below, so instances of this class should
 -- | not be defined in libraries.
+-- class TypeEquals :: forall k. k -> k -> Constraint
 class TypeEquals a b | a -> b, b -> a where
-  to :: a -> b
-  from :: b -> a
+  leibniz :: forall p. p a -> p b
 
 instance refl :: TypeEquals a a where
-  to a = a
-  from a = a
+  leibniz a = a
+
+newtype To a = To a
+
+to :: forall a b. TypeEquals a b => a -> b
+to a = case leibniz (To a) of
+  To b -> b
+
+newtype From a b = From (b -> a)
+
+from :: forall a b. TypeEquals a b => b -> a
+from b = case leibniz (From (\a -> a) :: From a a) of
+  (From f :: From a b) -> f b
