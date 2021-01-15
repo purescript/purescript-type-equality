66b8c45fdc76872b5282b681c38f512910723359
Nick Scheel <11701520+MonoidMusician@users.noreply.github.com>
2021-01-15 08:04:06 -0600

    Add Coercible constraint to test (#1)

--- test/Main.purs
+++ test/Main.purs
@@ -4,14 +4,16 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 import Data.Newtype (class Newtype, unwrap)
-import Type.Equality (class TypeEquals, proof, to, from)
+import Type.Equality (class TypeEquals, proof)
+import Prim.Coerce (class Coercible)
 
 newtype RecordNewtype = RecordNewtype
   { message :: String }
 
 instance newtypeRecordNewtype ::
-  TypeEquals inner { message :: String }
-    => Newtype RecordNewtype inner
+  (TypeEquals inner { message :: String }
+  , Coercible inner { message :: String }
+  ) => Newtype RecordNewtype inner
 
 message :: forall ty row. TypeEquals row ( message :: String ) => Newtype ty (Record row) => ty -> String
 message = unwrap >>> proof >>> _.message
