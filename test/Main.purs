b12ba68daa8e3e0ed2766697ede9d1ba59d8177e
Jordan Martinez <jordanalex.martinez@gmail.com>
2021-01-16 08:23:14 -0800

    Remove parenthesis

--- test/Main.purs
+++ test/Main.purs
@@ -10,8 +10,8 @@ newtype RecordNewtype = RecordNewtype
   { message :: String }
 
 instance newtypeRecordNewtype ::
-  (TypeEquals inner { message :: String }
-  ) => Newtype RecordNewtype inner
+  TypeEquals inner { message :: String }
+   => Newtype RecordNewtype inner
 
 message :: forall ty row. TypeEquals row ( message :: String ) => Newtype ty (Record row) => ty -> String
 message = unwrap >>> proof >>> _.message
