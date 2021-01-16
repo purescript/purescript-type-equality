cb8a49143af9912806702a365155c36800db1cdb
JordanMartinez <jordanalex.martinez@gmail.com>
2021-01-16 10:22:33 -0800

    Fix Newtype instance in test code (#14)
    * Remove wrap/unwrap from test code's Newtype instance
    
    * Add Coercible constraint to test (#1)
    
    * Make Coercible a super class of TypeEquals
    
    * Remove parenthesis
    
    Co-authored-by: Nick Scheel <11701520+MonoidMusician@users.noreply.github.com>

--- test/Main.purs
+++ test/Main.purs
@@ -4,16 +4,14 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 import Data.Newtype (class Newtype, unwrap)
-import Type.Equality (class TypeEquals, proof, to, from)
+import Type.Equality (class TypeEquals, proof)
 
 newtype RecordNewtype = RecordNewtype
   { message :: String }
 
 instance newtypeRecordNewtype ::
   TypeEquals inner { message :: String }
-    => Newtype RecordNewtype inner where
-  wrap = RecordNewtype <<< to
-  unwrap (RecordNewtype rec) = from rec
+   => Newtype RecordNewtype inner
 
 message :: forall ty row. TypeEquals row ( message :: String ) => Newtype ty (Record row) => ty -> String
 message = unwrap >>> proof >>> _.message
